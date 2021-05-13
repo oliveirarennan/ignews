461ec3fb37dfea94117355bfeedec61178ac449b
Rennan Oliveira <rennan_neves@live.com>
2021-05-13 16:37:48 -0300

    feat: :tada: initial commit

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { signIn, useSession } from "next-auth/client";
+import { getStripeJs } from "../../services/stripe-js";
+import styles from "./styles.module.scss";
+
+type SubscribeButtonProps = {
+  priceId: string,
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await axios.post('api/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = getStripeJs()
+
+      await (await stripe).redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  )
+}
